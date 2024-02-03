dfc05999e0f8d8330990bfa9c4c7bddbcfda12bd
FlanagG21 <104097148+FlanagG21@users.noreply.github.com>
2024-02-03 16:43:13 -0500

    Update textSender.py

--- textSender.py
+++ textSender.py
@@ -1,9 +1,11 @@
 import asyncio
 import websockets
+message = False
 
 
 async def text(websocket, path):
-    message = await websocket.recv()
+    global message
+    message = asyncio.wait_for(asyncio.create_task(websocket.recv()), timeout=2)
     await websocket.send(message)
 
 
