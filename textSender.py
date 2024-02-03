45d8ae9fc32adf965914709c2d7b48f3cb5ba633
MasonDiGi-VT <116042862+MasonDiGi-VT@users.noreply.github.com>
2024-02-03 16:54:53 -0500

    Update textSender.py

--- textSender.py
+++ textSender.py
@@ -1,13 +1,51 @@
 import asyncio
 import websockets
+
+nums = 0
+reply = ""
+
+
+# create handler for each connection
+async def handler(websocket, path):
+    global nums, reply
+    while True:
+        t2 = asyncio.create_task(websocket.recv())
+        data = None
+        try:
+            data = await asyncio.wait_for(t2, timeout=2)
+        except:
+            await asyncio.sleep(1)
+        if data and data == "T":
+            print("Clearing")
+            nums = 0
+        if nums > 10:
+            reply = "AAAA"
+        else:
+            reply = f"Hello {nums}"
+        nums += 1
+        print("sending " + reply)
+
+        await websocket.send(reply)
+
+
 message = False
 
 
 async def text(websocket, path):
     global message
-    message = asyncio.wait_for(asyncio.create_task(websocket.recv()), timeout=2)
-    await websocket.send(message)
+    while True:
+        task = asyncio.create_task(websocket.recv())
+        try:
+            message = await asyncio.wait_for(task, timeout=2)
+            print(message)
+        except:
+            pass
+        await websocket.send(message)
 
 
 start_server = websockets.serve(text, "localhost", 8000)
 
+
+asyncio.get_event_loop().run_until_complete(start_server)
+
+asyncio.get_event_loop().run_forever()
