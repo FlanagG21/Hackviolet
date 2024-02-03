e9ffb22568cecd66ae3e21b985b93cb8c6edacc1
FlanagG21 <104097148+FlanagG21@users.noreply.github.com>
2024-02-03 15:31:25 -0500

    Update textSender.py

--- textSender.py
+++ textSender.py
@@ -1,15 +1,10 @@
 import asyncio
 import websockets
-from twilio.rest import Client
-textSent = False
+
 
 async def text(websocket, path):
     message = await websocket.recv()
-    if message and not textSent:
-        await websocket.send("A fire alarm may be going off in your vicinity, check your suroundings")
-        textSent = True
-    elif not message:
-        textSent = False 
+    await websocket.send(message)
 
 
 start_server = websockets.serve(text, "localhost", 8000)
