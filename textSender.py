1af566b8e020a50418a3741431ed316e0e7caca6
FlanagG21 <104097148+FlanagG21@users.noreply.github.com>
2024-02-04 10:35:46 -0500

    Update textSender.py

--- textSender.py
+++ textSender.py
@@ -57,7 +57,7 @@ async def text(websocket, path):
             if message == "F" and not sent:
                 sender()
                 sent = True
-            else:
+            elif not message == "F":
                 sent = False
         except:
             pass
